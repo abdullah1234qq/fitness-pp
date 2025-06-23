ce10fd1fce2ac71a1dd8b0404158387ec64b0ab2
afz <abdullah1234qq>
2025-06-23 18:20:36 +0500

    Innitial Commit

--- src/components/pages/Welcome.vue
+++ src/components/pages/Welcome.vue
@@ -1,3 +1,18 @@
 <script setup></script>
-<template></template>
+<template>
+    <section id="welcome">
+        <div class="benefits">
+            <h2>Complete this Program <br /> if you want ...</h2>
+            <div>
+                <p>✅ Follow a simple program with proven results.</p>
+                <p>💪 Get fit, healthy, strong and absolutely shredded.</p>
+                <p>🏋 learn more about gym, training and technique</p>
+
+            </div>
+        </div>
+        <div>
+            <h3>Training Plan</h3>
+        </div>
+    </section>
+</template>
 <style scoped></style>
