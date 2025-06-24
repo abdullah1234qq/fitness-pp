80a92816d4449f8058339e3b821670f4cc700c26
afz <abdullah1234qq>
2025-06-24 14:39:31 +0500

    Innitial Commit

--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -34,6 +34,7 @@ footer {
     flex-direction: column;
     gap: 0.5rem;
     align-items: center;
-    padding: 3rem 0;
+    padding: 2rem 0;
+    padding-bottom: 3rem;
 }
 </style>
--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -1,7 +1,17 @@
 <script setup>
+import { gymHealthFacts } from "../../utils/next.js"
+
+const randomNumber = Math.floor(Math.random() * gymHealthFacts.length)
 </script>
 <template>
-
+    <section id="dashboard">
+        <div class="card tip-container">
+            <h2> Welcome to dashboard</h2>
+            <div>
+                <p class="tip"><strong>Daily Tip</strong><br />INJECT THE TIP</p>
+            </div>
+        </div>
+    </section>
 
 </template>
 <style scoped></style>
