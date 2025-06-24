b48c6c4bdc13f7ba4e67c96340af35e544d1312d
Abdullah Hussain <abdullah1234qq>
2025-06-24 17:13:56 +0500

    Innitial commit

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -17,11 +17,37 @@ const { workout = [], warmup = [] } = workoutProgram[selectedWorkout] || {};
             <h6>Sets</h6>
             <h6>Reps</h6>
             <h6 class="grid-weight">Weight</h6>
-            <div class="workout-grid" v-for="(w, wIdx) in warmup" key="wIdx">
+            <div class="workout-grid" v-for="(w, wIdx) in workout" key="wIdx">
                 <div class="grid-name">
                     <p>{{ w.name }}</p>
-                    <button><i class="fa-regular fa-circle-question"></i></button>
+                    <button>
+                        <i class="fa-regular fa-circle-question"></i>
+                    </button>
                 </div>
+                <p>{{ w.sets }}</p>
+                <p>{{ w.reps }}</p>
+                <input type="text" placeholder="14kg" class="grid-weight" v-model="w.weight">
+            </div>
+            <br />
+            <div class="workout-grid-line">
+                <h4 class="grid-name">Workout</h4>
+                <h6>Sets</h6>
+                <h6>Reps</h6>
+                <h6 class="grid-weight">Weight</h6>
+                <div class="workout-grid" v-for="(w, wIdx) in workout" key="wIdx">
+                    <div class="grid-name">
+                        <p>{{ w.name }}</p>
+                        <button>
+                            <i class="fa-regular fa-circle-question"></i>
+                        </button>
+                    </div>
+                    <p>{{ w.sets }}</p>
+                    <p>{{ w.reps }}</p>
+                    <input type="text" placeholder="14kg" class="grid-weight">
+                </div>
+                <br />
+
+                <div class="card"><button>Save and Exit <i class="fa-solid fa-save"></i></button></div>
             </div>
         </div>
     </section>
