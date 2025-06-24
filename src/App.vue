1d873b214265eb6389e6a11dc0b6a9a05f6713c7
afz <abdullah1234qq>
2025-06-24 12:39:54 +0500

    Innitial Commit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Welcome from './components/pages/Welcome.vue';
 
 <template>
     <div>
-        <h1 class="text-gradient">Hello World</h1>
+        <h1 class="text-gradient">Welcome to the Fitness App</h1>
     </div>
     <Welcome />
 </template>
--- src/components/pages/Welcome.vue
+++ src/components/pages/Welcome.vue
@@ -2,7 +2,7 @@
 <template>
     <section id="welcome">
         <div class="benefits">
-            <h2>Complete this Program <br /> if you want ...</h2>
+            <h2>Complete this Program <br /> if you want...</h2>
             <div>
                 <p>✅ Follow a simple program with proven results.</p>
                 <p>💪 Get fit, healthy, strong and absolutely shredded.</p>
@@ -12,7 +12,34 @@
         </div>
         <div>
             <h3>Training Plan</h3>
+            <p>This training plan follows a structure known as the <strong>Bro split</strong>.</p>
+            <h5><i> push &rarr; pull &rarr; Legs &rarr; Repeat</i></h5>
+        </div>
+        <div class="card cchallenge">
+            <h3>🔥 Your Challenge</h3>
+            <p>Complete all the workouts and track your progress along all way!</p>
+            <p>Do you accept?</p>
+            <button>Begin &rarr;</button>
         </div>
     </section>
 </template>
-<style scoped></style>
+<style scoped>
+#welcome,
+.challenge,
+.benefits {
+    display: flex;
+    flex-direction: column;
+}
+
+#welcome {
+    gap: 1.2rem;
+}
+
+.benefits {
+    gap: 0.5rem;
+}
+
+.challenge {
+    gap: 0.25rem;
+}
+</style>
