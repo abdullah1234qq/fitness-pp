a7c92f4d5f3ce95b877aec198a404e3d9a460eea
Abdullah Hussain <abdullah1234qq>
2025-06-24 17:42:40 +0500

    Innitial commit

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -29,7 +29,7 @@ const { workout = [], warmup = [] } = workoutProgram[selectedWorkout] || {};
                 <input type="text" placeholder="14kg" class="grid-weight" v-model="w.weight">
             </div>
             <br />
-            <div class="workout-grid-line">
+            <div class="workout-grid">
                 <h4 class="grid-name">Workout</h4>
                 <h6>Sets</h6>
                 <h6>Reps</h6>
@@ -43,16 +43,57 @@ const { workout = [], warmup = [] } = workoutProgram[selectedWorkout] || {};
                     </div>
                     <p>{{ w.sets }}</p>
                     <p>{{ w.reps }}</p>
-                    <input type="text" placeholder="14kg" class="grid-weight">
-                </div>
-                <br />
-
-                <div class="card">
-                    <button>Save and Exit <i class="fa-solid fa-save"></i></button>
-                    <button>Complete <i class="fa-solid fa-check"></i></button>
+                    <input type="text" placeholder="14kg" class="grid-weight" v-model="w.weight">
                 </div>
             </div>
         </div>
     </section>
 </template>
-<style scoped></style>
+<style scoped>
+#workout-card,
+.plan-card {
+    display: flex;
+    flex-direction: column;
+
+}
+
+#workout-card {
+    gap: 1.5rem;
+}
+
+.plan-card-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1rem;
+}
+
+.workout-grid,
+.workout-grid-row {
+    display: grid;
+    grid-template-columns: repeat(7, minmax(0, 1fr));
+    gap: 1rem;
+}
+
+.workout-grid,
+.workout-grid-line {
+    grid-column: span 7 / span 7;
+    gap: 1 rem;
+}
+
+.grid-name {
+    grid-column: span 3 / span 3;
+}
+
+.grid-name button {
+    padding: 0;
+    border: none;
+    box-shadow: none;
+}
+
+.grid-name button:hover {
+    transform: none;
+    box-shadow: none;
+    color: var(--color-link)
+}
+</style>
