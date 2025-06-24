1ccfbe33b433a9e3d494cee0e543133f1dd218c4
Abdullah Hussain <abdullah1234qq>
2025-06-24 14:58:49 +0500

    Innitial commit

--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -1,17 +1,33 @@
 <script setup>
 import { gymHealthFacts } from "../../utils/next.js"
+import Grid from "../../components/Grid.vue"
 
 const randomNumber = Math.floor(Math.random() * gymHealthFacts.length)
+const todaysFact = gymHealthFacts[randomNumber]
+
 </script>
 <template>
     <section id="dashboard">
         <div class="card tip-container">
             <h2> Welcome to dashboard</h2>
             <div>
-                <p class="tip"><strong>Daily Tip</strong><br />INJECT THE TIP</p>
+                <p class="tip"><strong>Daily Tip</strong><br />{{ todaysFact }}</p>
             </div>
+            <button>Start workout &rarr;</button>
         </div>
+        <Grid />
     </section>
 
 </template>
-<style scoped></style>
+<style scoped>
+.tip-container,
+.tip-container div,
+#dashboard {
+    display: flex;
+}
+
+.tip-container,
+#dashboard {
+    flex-direction: column;
+}
+</style>
