0b39e42e70edc93566c2a1f77d44f2003de30da0
Abdullah Hussain <abdullah1234qq>
2025-06-24 15:16:46 +0500

    Innitial commit

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,3 +1,16 @@
-<script setup></script>
-<template></template>
+<script setup>
+import { workoutProgram } from "../utils/next.js"
+const workoutTypes = ["push", "pull", "legs"]
+</script>
+<template>
+    <section id="grid">
+        <button v-for="workout in Object.keys(workoutProgram)" class="card-button plan-card">
+            <div class="">
+                <p>Day 1</p>
+                ICON
+            </div>
+            <h3></h3>
+        </button>
+    </section>
+</template>
 <style scoped></style>
